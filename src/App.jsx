61987532d85a1227e84e4b698bad4a384c9cbf52
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-25 09:24:47 -0500

    Standart Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import Map from './Components/Map'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,18 +8,6 @@ function App() {
   return (
     <>
       <Map />
-      {/* <h1 className="text-3xl font-bold underline relative">
-      Hello world!
-      </h1>
-      <div className='flex'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-
     </>
   )
 }
